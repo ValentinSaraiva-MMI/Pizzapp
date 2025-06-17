95fd42eefb40030d2c2bb579204e47c9ecea4b28
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2025-06-17 15:40:09 +0200

    Add Pizza and PizzaBuilder classes with builder pattern implementation

--- builderPattern/PizzaBuilder.ts
+++ builderPattern/PizzaBuilder.ts
@@ -1,1 +1,35 @@
-class Pizza {}
+import { Pizza } from "../models/Pizza";
+
+export class PizzaBuilder {
+  private pate: string = "";
+  private sauce: string = "";
+  private taille: string = "";
+  private garnitures: string[] = [];
+
+  setTaille(taille: string): PizzaBuilder {
+    this.taille = taille;
+    return this;
+  }
+
+  setPate(pate: string): PizzaBuilder {
+    this.pate = pate;
+    return this;
+  }
+
+  setSauce(sauce: string): PizzaBuilder {
+    this.sauce = sauce;
+    return this;
+  }
+
+  addGarniture(garniture: string): PizzaBuilder {
+    this.garnitures.push(garniture);
+    return this;
+  }
+
+  build() {
+    if (!this.pate || !this.sauce || !this.taille) {
+      throw new Error("Pâte,sauce et taille doivent être définies.");
+    } else
+      return new Pizza(this.pate, this.sauce, this.taille, this.garnitures);
+  }
+}
---
+++ models/Pizza.ts
@@ -0,0 +1,34 @@
+export class Pizza {
+  public pate: string;
+  public sauce: string;
+  public taille: string;
+  public garnitures: string[];
+
+  constructor(
+    pate: string,
+    sauce: string,
+    taille: string,
+    garnitures: string[]
+  ) {
+    this.pate = pate;
+    this.sauce = sauce;
+    this.taille = taille;
+    this.garnitures = garnitures;
+  }
+
+  cuire(): void {
+    console.log("Cuisson de la pizza");
+  }
+
+  emballer(): void {
+    console.log("Emballage de la pizza.");
+  }
+
+  afficher(): void {
+    console.log(
+      `Pizza avec pâte ${this.pate}, sauce ${
+        this.sauce
+      }, garnitures : ${this.garnitures.join(", ")}`
+    );
+  }
+}
